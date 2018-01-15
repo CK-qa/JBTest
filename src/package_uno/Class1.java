160df9138f7b7c6b8a7cf9c1e2a371de13076d8d
viktoria.bozhko <victoria.bozhko@gmail.com>
2018-01-15 20:03:20 +0300

    first commit

---
+++ src/package_uno/Class1.java
@@ -0,0 +1,20 @@
+package package_uno;
+
+public class Class1 extends Class2{
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i + " " + str());
+        }
+    }
+
+    public Class1(int i) {
+        super(i);
+    }
+
+    public int iii(int o) {
+        return o;
+    }
+
+}
---
+++ src/package_uno/Class2.java
@@ -0,0 +1,13 @@
+package package_uno;
+
+class Class2 {
+    private int it;
+
+    Class2(int i) {
+        this.it = i;
+    }
+
+    static String str() {
+        return "string";
+    }
+}
