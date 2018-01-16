1e9ae6dacfb22c90501ba10b4d802725fb3a485e
viktoria.bozhko <victoria.bozhko@gmail.com>
2018-01-16 15:17:14 +0300

    file added

---
+++ src/TestBug.java
@@ -0,0 +1,14 @@
+public class TestBug {
+
+    public static String str() {
+        return "p";
+    }
+
+    public static void doStr() {
+        str();
+    }
+
+    public static void main(String[] args) {
+        doStr();
+    }
+}
